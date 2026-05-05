c15f6bad93a37abce9f83412eee8157580bbb827
hackertron <hackertron@unitendo.org>
2026-05-05 16:47:02 -0500

    uni

--- plugin/src/config.cpp
+++ plugin/src/config.cpp
@@ -205,7 +205,7 @@ void Config::Init() {
     WUPSConfigAPIStatus cres;
 
     // Init the config api
-    WUPSConfigAPIOptionsV1 configOptions = { .name = "Inkay" };
+    WUPSConfigAPIOptionsV1 configOptions = { .name = "Inkay-universe" };
     cres = WUPSConfigAPI_Init(configOptions, ConfigMenuOpenedCallback, ConfigMenuClosedCallback);
     if (cres != WUPSCONFIG_API_RESULT_SUCCESS) return (void)report_error(cres);
 
