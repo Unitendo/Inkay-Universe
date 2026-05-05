e5ba6d0510d662c707924b53e0a3e0a418a2a561
hackertron <hackertron@unitendo.org>
2026-05-05 16:48:00 -0500

    cool

--- plugin/src/main.cpp
+++ plugin/src/main.cpp
@@ -29,10 +29,10 @@
     Mandatory plugin information.
     If not set correctly, the loader will refuse to use the plugin.
 **/
-WUPS_PLUGIN_NAME("Inkay");
-WUPS_PLUGIN_DESCRIPTION("Pretendo Network Patcher");
+WUPS_PLUGIN_NAME("Inkay-universe");
+WUPS_PLUGIN_DESCRIPTION("Pretendo Network/Universe Patcher");
 WUPS_PLUGIN_VERSION(INKAY_VERSION);
-WUPS_PLUGIN_AUTHOR("Pretendo contributors");
+WUPS_PLUGIN_AUTHOR("Pretendo contributors + cool guy");
 WUPS_PLUGIN_LICENSE("GPLv3");
 
 WUPS_USE_STORAGE("inkay");
