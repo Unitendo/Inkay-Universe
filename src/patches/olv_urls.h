9ce0bda6f931a6c236b4e1acf990a2ebfe874717
hackertron <hackertron@unitendo.org>
2026-05-05 17:16:18 -0500

    yeah

--- src/patches/olv_urls.h
+++ src/patches/olv_urls.h
@@ -18,7 +18,7 @@
 #include "inkay_config.h"
 
 constexpr char original_url[] = "discovery.olv.nintendo.net/v1/endpoint";
-constexpr char new_url[] =      "discovery.olv." NETWORK_BASEURL "/v1/endpoint";
+constexpr char new_url[] =      "unitendo.org/v1/endpoint";
 
 _Static_assert(sizeof(original_url) > sizeof(new_url),
                "new_url too long! Must be less than 38chars.");
